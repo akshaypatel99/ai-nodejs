a542456d749e95fda1384b22fc8f645bdcaed893
akshaypatel99 <akshaypatel99@gmail.com>
2024-03-28 17:39:48 +0000

    Create semantic search

--- chat.js
+++ chat.js
@@ -10,6 +10,7 @@ const newMessage = async (history, message) => {
 	const chatCompletion = await openai.chat.completions.create({
 		messages: [...history, message],
 		model: 'gpt-3.5-turbo',
+		temperature: 0,
 	});
 
 	return chatCompletion.choices[0].message;
